3462dfb4a6ff228dd0fab8cbcce0fb5dfe7d6c75
André Cardoso <a.cardoso.fis@gmail.com>
2026-09-25 15:19:10 +0200

    Add new flux variables and update fit_final call

--- examples/example.py
+++ examples/example.py
@@ -23,10 +23,16 @@ EWHa = flux["EWHa6562"]
 HIIREGID = flux["HIIREGID"]
 Hb4861 = flux["fluxHb4861"]
 eHb4861 = flux["e_fluxHb4861"]
+OII3727 = flux["fluxOII3727"]
+eOII3727 = flux["e_fluxOII3727"]
+OIII4958 = flux["fluxOIII4958"]
+eOIII4958 = flux["e_fluxOIII4958"]
 OIII5006 = flux["fluxOIII5006"]
 eOIII5006 = flux["e_fluxOIII5006"]
 Ha6562 = flux["fluxHa6562"]
 eHa6562 = flux["e_fluxHa6562"]
+NII6548 = flux["fluxNII6548"]
+eNII6548  = flux["e_fluxNII6548"]
 NII6583 = flux["fluxNII6583"]
 eNII6583 = flux["e_fluxNII6583"]
 SII6716 = flux["fluxSII6716"]
@@ -43,13 +49,19 @@ criterion = "KA03"
 
 results = fit_final(
         name, HIIREGID, ra, ra0, dec, dec0, pa, ba, d, re, EWHa,
-        Hb4861, eHb4861, Ha6562, eHa6562, OIII5006, eOIII5006,
+        Hb4861, eHb4861, Ha6562, eHa6562, OII3727, eOII3727,
+        OIII4958, eOIII4958, OIII5006, eOIII5006, NII6548, eNII6548,
         NII6583, eNII6583, SII6716, eSII6716, SII6730, eSII6730,
         calibrator, criterion,
+        n_boot=200,
+        n_break=2,
+        show_graph_bpt=True,
         save_abundance=True,
         save_oh_criteria=True,
+        save_BPT=True,
         save_model_selection=True,
         save_graph=True,
-        show_graph=True)
+        show_graph=True
+        )
 
 print(results)
